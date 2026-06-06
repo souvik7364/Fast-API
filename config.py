5182b6284607de7a511cde0cfcd02dc9477830b6
Souvik Pal <souvik912006@gmail.com>
2026-06-06 18:21:05 +0530

    fixed config to read from env variables

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 load_dotenv()
 
-db_url = "mysql+pymysql://root:Souvik.2006@localhost:3306/fastapi_db"
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+db_url = os.getenv("db_url")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
